b3af3ba79c64e4f199f92c68e8603abce56260e8
Mauricio Coelho <maudrico9@gmail.com>
2023-01-22 23:13:46 -0300

    v1 api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\v1\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
